49766a1b63bf77460eb73b89175a7534f92e624d
preete126 <ismailbarakatopeyemi05@gmail.com>
2023-04-01 14:10:44 +0100

    fix: fixed conflict issues

--- src/pages/home/main.jsx
+++ src/pages/home/main.jsx
@@ -10,9 +10,6 @@ import bg from '../../assets/images/luxuryproperty.avif'
 import bg1 from '../../assets/images/building-n-h.jpg'
 import { ArrowRightOutlined,  HighlightOutlined} from '@ant-design/icons'
 import Projects from '../../components/projects'
-import bgdraw from '../../assets/images/artchitecturedraw.avif'
-import buildings from '/src/assets/images/buildings.avif'
-import { ArrowRightOutlined, CheckOutlined, FallOutlined, HighlightOutlined, MailFilled, SmileOutlined } from '@ant-design/icons'
 
 function Main() {
     const [count, setCount] = useState(0)
@@ -28,7 +25,6 @@ function Main() {
       */}
             <section className='dbimg ' style={{ backgroundImage: `url(${bg1})`, }}>
 
-            <section className='dbimg' style={{ backgroundImage: `url(${bg1})`, }}>
                 {/* <div className='logo'>
                     <img className='w-100' src={logo} alt="" />
 
@@ -51,161 +47,6 @@ function Main() {
             </section>
            
            <Projects/>
-            <section className='safety d-flex p'>
-                <main>
-                    <img src={buildings} alt="" />
-                </main>
-
-                <div className='gy-2'>
-                    <div className='construction '>
-                        CONSTUCTURE PROJECTS AND TECHNOLOGY INFRASTRUCTURE
-                    </div>
-                    <h3 className=''>Safety, quality and sustainablility</h3>
-                    <div className='at'>At Kolad consult ltd, we have helped customers complete more than 1,000 projects,
-                        in countries that have created jobs and grown economies, improved the resiliency of the
-                        world's infrastructure, increased access to resources and vital services, and made the world a safer place.
-                    </div>
-                    <div className='d-flex list gy-3'>
-
-                        <div className=' d-flex w-50 gap-3'>
-                            <span className='d-flex '><CheckOutlined className='my-auto text-warning' /></span>
-                            <strong className=''> Constructural Technology</strong>
-                        </div>
-                        <div className=' d-flex  w-50 gap-3'>
-                            <span className='d-flex'><CheckOutlined className='my-auto text-warning' /></span>
-                            <strong className=''> Metallurgical & Material</strong>
-                        </div>
-
-                        <div className=' d-flex w-50 gap-3'>
-                            <span className='d-flex '><CheckOutlined className='my-auto text-warning' /></span>
-                            <strong className=''>Heavy Engineering</strong>
-                        </div>
-                        <div className=' d-flex  w-50 gap-3'>
-                            <span className='d-flex'><CheckOutlined className='my-auto text-warning' /></span>
-                            <strong className=''>Electrical & Automation</strong>
-                        </div>
-
-                        <div className=' d-flex w-50 gap-3'>
-                            <span className='d-flex '><CheckOutlined className='my-auto text-warning' /></span>
-                            <strong className=''>Architectural Design</strong>
-                        </div>
-                        <div className=' d-flex  w-50 gap-3'>
-                            <span className='d-flex'><CheckOutlined className='my-auto text-warning' /></span>
-                            <strong className=''>Interior & Exterior Design</strong>
-                        </div>
-                    </div>
-                    <button className='btnget d-flex'>
-                        <div>Our Service</div>
-                    </button>
-                </div>
-            </section>
-
-            <section className=' we'>
-                <div className='mx-auto'>
-                    <div className='construction '>
-                        WE REPRESENT THE FUTURE OF CONSTRUCTION
-                    </div>
-                    <h3>Our customers' projects are an investment in the future</h3>
-                    <div className='Diffrientiated'>
-                        Diffrientiated by the quality of our people and our relentless drive to deliver the most successful
-                        outcomes, we relign our capabilities to our customers' objectives to create a positive impact.
-                    </div>
-
-                    <div className=' d-fle listimg'>
-                        <div style={{ backgroundImage: `url(${bg})`, }} className=''>
-                            <section className='cardimg1 '>
-                                <SmileOutlined />
-                                <h5>Engineering</h5>
-                                <hr className='divider' />
-                                <section>
-                                    In the constructural constructural industry, experience counts. We offer total project
-                                    solutions and integrating our expertise in all areas of the construction process.
-                                </section>
-                                <a href="">Learn More
-                                    <ArrowRightOutlined className='my-auto ' />
-                                </a>
-                            </section>
-                        </div>
-                        <div style={{ backgroundImage: `url(${bg})`, }} className=''>
-                            <section className='cardimg2 '>
-                                <SmileOutlined />
-                                <h5>Construction</h5>
-                                <hr className='divider' />
-                                <section>
-                                    In the constructural constructural industry, experience counts. We offer total project
-                                    solutions and integrating our expertise in all areas of the construction process.
-                                </section>
-                                <a href="">Learn More
-                                    <ArrowRightOutlined className='my-auto ' />
-                                </a>
-                            </section>
-                        </div>
-                        <div style={{ backgroundImage: `url(${bg})`, }} className=''>
-                            <section className='cardimg3 '>
-                                <SmileOutlined />
-                                <h5>Construction</h5>
-                                <hr className='divider' />
-                                <section>
-                                    In the constructural constructural industry, experience counts. We offer total project
-                                    solutions and integrating our expertise in all areas of the construction process.
-                                </section>
-                                <a href="">Learn More
-                                    <ArrowRightOutlined className='my-auto ' />
-                                </a>
-                            </section>
-                        </div>
-
-                        <div style={{ backgroundImage: `url(${bg})`, }} className=''>
-                            <section className='cardimg4 '>
-                                <SmileOutlined />
-                                <h5>Construction</h5>
-                                <hr className='divider' />
-                                <section>
-                                    In the constructural constructural industry, experience counts. We offer total project
-                                    solutions and integrating our expertise in all areas of the construction process.
-                                </section>
-                                <a href="">Learn More
-                                    <ArrowRightOutlined className='my-auto ' />
-                                </a>
-                            </section>
-                        </div>
-                        <div style={{ backgroundImage: `url(${bg})`, }} className=''>
-                            <section className='cardimg5 '>
-                                <SmileOutlined />
-                                <h5>Construction</h5>
-                                <hr className='divider' />
-                                <section>
-                                    In the constructural constructural industry, experience counts. We offer total project
-                                    solutions and integrating our expertise in all areas of the construction process.
-                                </section>
-                                <a href="">Learn More
-                                    <ArrowRightOutlined className='my-auto ' />
-                                </a>
-                            </section>
-                        </div>
-                        <div style={{ backgroundImage: `url(${bg})`, }} className=''>
-                            <section className='cardimg6 '>
-                                <SmileOutlined className='ms-auto' />
-
-                                <h5>Construction</h5>
-                                <hr className='divider' />
-                                <section>
-                                    In the constructural constructural industry, experience counts. We offer total project
-                                    solutions and integrating our expertise in all areas of the construction process.
-                                </section>
-                                <a href="">Learn More
-                                    <ArrowRightOutlined className='my-auto ' />
-                                </a>
-                            </section>
-                        </div>
-                        <section>
-                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus unde id iste cum esse quam iure ratione repellendus corrupti vitae. Quos magnam unde ipsam asperiores assumenda quod fugit, voluptates animi.
-                        </section>
-
-                    </div>
-
-                </div>
-            </section>
 
             <section className='usbgimg' style={{ backgroundImage: `url(${bg})`, }}>
                 <div className='gy-2 gap-3'>
@@ -234,12 +75,7 @@ function Main() {
                 </main>
             </section>
 
-            <section style={{ backgroundImage: `url(${bgdraw})` }} className='containn d-flex p'>
-
-
-                {/* <h2 className='logo bg-danger'>wggkadgkacgkascgag</h2> */}
-
-
+            <section className='safety d-flex p'>
             </section>
 
         </div>
