1ac312f20ca731761e0a5de97de6a67f261749bb
ismail habeeb <ismailhabeeboluwatobi995@gmail.com>
2023-04-22 20:02:06 +0100

    fix

--- src/components/project_preview/index.jsx
+++ src/components/project_preview/index.jsx
@@ -1,37 +1,37 @@
 import { ClockCircleOutlined, EnvironmentFilled } from "@ant-design/icons";
 import single from './images/pa2.jpg'
 // images//
-import a1 from './images/p3db1.jpg'
-import a2 from './images/p3db2.jpg'
-import a3 from './images/p3db3.jpg'
-import a4 from './images/p3db4.jpg'
-import a5 from './images/p3db5.jpg'
-// 2
-import b1 from './images/p3dc1.jpg'
-import b2 from './images/p3dc2.jpg'
-//3
-import c1 from './images/p3dd1.jpg'
-import c2 from './images/p3dd2.jpg'
-import c3 from './images/p3dd3.jpg'
-import c4 from './images/p3dd4.jpg'
-//general img
-import pr1 from './images/ppr3d1.jpg'
-import pr2 from './images/ppr3d2.jpg'
-import pr3 from './images/pprgen.jpg'
-import pr4 from './images/pprgen2.jpg'
-import pr5 from './images/pprgen3.jpg'
+// import a1 from './images/p3db1.jpg'
+// import a2 from './images/p3db2.jpg'
+// import a3 from './images/p3db3.jpg'
+// import a4 from './images/p3db4.jpg'
+// import a5 from './images/p3db5.jpg'
+// // 2
+// import b1 from './images/p3dc1.jpg'
+// import b2 from './images/p3dc2.jpg'
+// //3
+// import c1 from './images/p3dd1.jpg'
+// import c2 from './images/p3dd2.jpg'
+// import c3 from './images/p3dd3.jpg'
+// import c4 from './images/p3dd4.jpg'
+// //general img
+// import pr1 from './images/ppr3d1.jpg'
+// import pr2 from './images/ppr3d2.jpg'
+// import pr3 from './images/pprgen.jpg'
+// import pr4 from './images/pprgen2.jpg'
+// import pr5 from './images/pprgen3.jpg'
 import { useState } from "react";
 
 
 function Project_preview(params) {
-    const [project1, setProject1] = useState(a1, a2, a3, a4, a5, pr1, pr2, pr3, pr4, pr5,)
-    const [project2, setProject2] = useState(b1, b2, pr1, pr2, pr3, pr4, pr5,)
-    const [project3, setProject3] = useState(c1, c2, c3, c4, pr1, pr2, pr3, pr4, pr5,)
+    // const [project1, setProject1] = useState(a1, a2, a3, a4, a5, pr1, pr2, pr3, pr4, pr5,)
+    // const [project2, setProject2] = useState(b1, b2, pr1, pr2, pr3, pr4, pr5,)
+    // const [project3, setProject3] = useState(c1, c2, c3, c4, pr1, pr2, pr3, pr4, pr5,)
 
 
     return (
         <section className="menu p py-4 " id="menu">
-            <div className="box-container" id="menu-items">
+            {/* <div className="box-container" id="menu-items">
                 <div id="ad${i}" className="box card">
                     <img loading="lazy" src={a1} alt="" />
                     <div className="content w-100">
@@ -109,7 +109,7 @@ function Project_preview(params) {
                     </div>
                 </div>
 
-            </div>
+            </div> */}
             <hr className='divider' />
             <div className="box-container" id="menu-items">
 
